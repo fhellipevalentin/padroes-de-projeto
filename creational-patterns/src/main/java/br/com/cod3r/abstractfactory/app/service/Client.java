67b2e373c06bb9689ad30eaaef95ee42e07d6280
fhellipevalentin <fhellipereis@outlook.com>
2023-09-27 19:23:56 -0300

    estudo sobre padrao abstract factory concluido

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
@@ -1,8 +1,21 @@
 package br.com.cod3r.abstractfactory.app.service;
 
+import br.com.cod3r.abstractfactory.app.service.factory.RestAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.ServicesAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
 public class Client {
 
 	public static void main(String[] args) {
-
+		
+		ServicesAbstractFactory factory = new RestAbstractFactory();
+		UserService usrService = factory.getUserService();
+		usrService.save("Jhon");
+		usrService.delete(5);
+		
+		CarService carService = factory.getCarService();
+		carService.save("Fiat Uno Mille");
+		carService.update("Corsa");
 	}
 }
---
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/factory/abstractFactory/USRulesAbstractFactory.java
@@ -0,0 +1,22 @@
+package br.com.cod3r.abstractfactory.apple.factory.abstractFactory;
+
+import br.com.cod3r.abstractfactory.apple.model.certificate.Certificate;
+import br.com.cod3r.abstractfactory.apple.model.certificate.USCertificate;
+import br.com.cod3r.abstractfactory.apple.model.packing.Packing;
+import br.com.cod3r.abstractfactory.apple.model.packing.USPacking;
+
+public class USRulesAbstractFactory implements CountryRulesAbstractFactory{
+
+	@Override
+	public Certificate getCertificates() {
+		// TODO Auto-generated method stub
+		return new USCertificate();
+	}
+
+	@Override
+	public Packing getPacking() {
+		// TODO Auto-generated method stub
+		return new USPacking();
+	}
+
+}
